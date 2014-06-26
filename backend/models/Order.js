91e286ec33bc7bcf5ba364686750cf7d194c33f1
naimdjon <naimdjon@gmail.com>
2014-06-27 00:12:05 +0200

    order mongoose model.

---
+++ backend/models/Order.js
@@ -0,0 +1,10 @@
+
+Order=mongoose.model('Order',
+    new Schema({
+        name: { type: String, required: true, trim: true }
+       ,mobile: {type: String, required: true, trim: true }
+       ,licencePlate: {type: String, required: true, trim: true }
+       ,comments: {type: String, required: false, trim: true }
+       ,orderDate:{type:Date}
+    })
+);
