62949f1c1678d16bb565c1a025a1a196cdc133ce
naimdjon <naimdjon@gmail.com>
2014-06-29 12:32:18 +0200

    order persisting to db.

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,26 +1,22 @@
 exports.submitOrder = function (request, response) {
-    var name = request.body.name;
-    var licencePlate = request.body.licencePlate;
-    var mobile = request.body.mobile;
-    var orderDate = request.body.orderDate;
-    var order = new Order;
-    order.name = name;
-    order.licencePlate = licencePlate;
-    order.mobile = mobile;
-    order.orderDate = orderDate;
+    console.dir("----   submitOrder -----");
+    console.dir(request.body);
+    console.log("name:"+request.body.name);
+    console.log("mobile:"+request.body.mobile);
+    console.log("nomer:"+request.body.licencePlate);
+    console.log("orderDate:"+request.body.orderDate);
+   var order = new Order;
+    order.name = request.body.name;
+    order.licencePlate = request.body.licencePlate;
+    order.mobile = request.body.mobile;
+    order.orderDate = request.body.orderDate;
     order.save(function (err, order, numberAffected) {
         if (err) {
             console.log("error:::::::"+err);
-            response.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'  });
-            response.end(JSON.stringify({"error": err}), 200);
+            response.jsonp(400,{error:"missing fields"});
             return;
         }
-        response.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'  });
-        if (parseInt(numberAffected) != 1) {
-            response.end(JSON.stringify({"error": "Could not insert the item:"+error+", " + numberAffected}), 500);
-        }else{
-            response.end(JSON.stringify({"ok":"item stored!"}, 200));
-        }
+        response.jsonp(400,{result:"ok"});
     });
 
 }
